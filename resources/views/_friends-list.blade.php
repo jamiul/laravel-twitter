d2ca037d066c75527848318f435410d7c22f2cf3
jamiul <jamiulewu24@gmail.com>
2020-08-24 10:09:08 +0600

    bulid a following

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,14 +1,16 @@
-<h3 class="font-bold text-xl mb-4">Frinds</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @foreach(range(1, 10) as $index)
+    @foreach(auth()->user()->follows as $user)
     <li class="mb-4">
         <div class="flex items-center">
             <img
-            src="https://i.pravatar.cc/40"
+            src="{{ $user->avatar }}"
             alt=""
             class="rounded-full mr-2"
             >
-            Jamiul Alam
+
+
+            {{ $user->name }}
         </div>
     </li>
     @endforeach
